cd27f5bda4b350d65e650b0005360c05ebd9ab9c
nguyendacthienngan <nguyendacthienngan@gmail.com>
2020-12-02 14:39:52 +0700

    Update port listen

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const loginRoute = require("./routes/login.route");
 const { body } = require("express-validator");
 
 const app = express();
-const port = 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -14,6 +13,4 @@ app.get("/", function(req,res) {
     res.send("Welcome to this api");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
-})
+app.listen(process.env.PORT || 8000);
