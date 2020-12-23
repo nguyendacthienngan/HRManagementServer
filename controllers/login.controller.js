4f748693737b402d85eb69960da12268e14d65d2
Bach Nguyen <nlebachnlb.develop@gmail.com>
2020-12-23 21:25:04 +0700

    Authentication: Login (no token)

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,8 +1,37 @@
 const http = require("../utils/http-status");
+const db = require("../models");
+const Account = db.Account;
+const employeeController = require("../controllers/employees.controller")
 
 module.exports.doLogin = (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
-    res.send(username);
-    console.log(password);
+    Account.findOne({
+        attributes: ["id", "username", "password", "employee_id"],
+        include: [{ model: db.Employee, required: true }],
+        where: {
+            username: username,
+            password: password
+        }
+    })
+        .then(result => {
+            if (!result) {
+                const authData = {
+                    message: "Invalid username or password"
+                }
+                return res.status(http.UNAUTHORIZED).json(authData);
+            }
+
+            const data = {
+                message: "Login successfully",
+                employee_id: result.employee_id,
+                employee_name: result.Employee.first_name + " " + result.Employee.last_name
+            }
+
+            res.status(http.ACCEPTED).json(data);
+        })
+        .catch(err => {
+            if (!err.status) err.statusCode = http.INTERNAL_SERVER_ERROR;
+            next(err);
+        })
 }
--- routes/login.route.js
+++ routes/login.route.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/login.controller")
 const { body } = require("express-validator");
 const api = require("./../utils/api-routes")
 
-
 router.post(api.actions.login, authController.doLogin);
+
 module.exports = router;
